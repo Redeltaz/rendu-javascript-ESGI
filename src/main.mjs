b0ebe364828751b9ae455ac51e869138811c2f93
Quozul <oxem@icloud.com>
2022-09-29 12:47:45 +0200

    remove alert

--- src/main.mjs
+++ src/main.mjs
@@ -1,15 +1,5 @@
-/*-----------------------*/
 import { CustomElement } from "./components/CustomElement/CustomElement.mjs";
 import "./scss/main.scss";
-/*-----------------------*/
 
 // Register custom elements here
 customElements.define("custom-element", CustomElement);
-
-alert("test");
-
-function test() {
-    console.log("test");
-}
-
-test();
