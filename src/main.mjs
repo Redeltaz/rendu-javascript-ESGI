f8412c78d33be7b3fa2273dc12a2a07d2c11c1e8
Quozul <oxem@icloud.com>
2022-09-29 10:27:47 +0200

    first web component

--- src/main.mjs
+++ src/main.mjs
@@ -1,6 +1,10 @@
 /*-----------------------*/
+import { CustomElement } from "./components/CustomElement/CustomElement.mjs";
 import "./scss/main.scss";
 /*-----------------------*/
+
+// Register custom elements here
+customElements.define("custom-element", CustomElement);
 
 alert("test");
 
