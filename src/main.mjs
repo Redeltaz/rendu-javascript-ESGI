afad79e8395aa26bf2d8fa97baed5e82246f32ea
Leo <bruantleo@gmail.com>
2023-01-02 18:22:59 +0100

    Addded home button #27

--- src/main.mjs
+++ src/main.mjs
@@ -4,11 +4,13 @@ import { AppIcon } from "./components/AppIcon/AppIcon.mjs";
 import { Calculator } from "./components/Calculator/Calculator.mjs";
 import { Container } from "./components/Container/Container.mjs";
 import { CustomElement } from "./components/CustomElement/CustomElement.mjs";
+import { HomeButton } from "./components/HomeButton/HomeButton.mjs";
 import { NavBar } from "./components/NavBar/NavBar.mjs";
 
 // Register custom elements here
-customElements.define("custom-element", CustomElement);
-customElements.define("nav-bar", NavBar);
 customElements.define("app-icon", AppIcon);
 customElements.define("custom-calculator", Calculator);
 customElements.define("custom-container", Container);
+customElements.define("custom-element", CustomElement);
+customElements.define("home-button", HomeButton);
+customElements.define("nav-bar", NavBar);
