693100e6db3868137a5ade1e03f22563488cd68e
Redeltaz <lucascampistron95@gmail.com>
2023-01-18 17:32:58 +0100

    feat: add html and css basic for tictactoe

--- src/components/TicTacToe/TicTacToe.mjs
+++ src/components/TicTacToe/TicTacToe.mjs
@@ -9,6 +9,21 @@ export class TicTacToe extends HTMLElement {
         const shadow = this.attachShadow({ mode: "open" });
         shadow.innerHTML = html;
 
+        const style = document.createElement("style");
+        style.textContent = css;
+        shadow.appendChild(style);
+
+        const buttons = shadow.querySelectorAll(".game-button")
+        const players = ["X", "O"]
+        this.turn = 0
+
+        console.log(buttons)
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.innerHTML = this.turn % 2 === 0 ? "X" : "O"
+                this.turn++
+            })
+        })
     }
 
 }
