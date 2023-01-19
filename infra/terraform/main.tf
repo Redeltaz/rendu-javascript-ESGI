148d6fb8e724d3d431fa3345c7668b4827b149d7
Redeltaz <lucascampistron95@gmail.com>
2023-01-19 12:12:42 +0100

    fix: change infra and add eip

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -24,7 +24,6 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_cidr_block
   availability_zone       = var.subnet_az
-  map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.vpc_name}-public-subnet"
@@ -143,4 +142,8 @@ resource "aws_instance" "ec2_instance" {
   tags = {
     Name = "rendu-js-instance"
   }
+}
+
+resource "aws_eip" "ec2_eip" {
+  instance = aws_instance.ec2_instance.id
 }
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "ec2_public_ip" {
-  value = aws_instance.ec2_instance.public_ip
+  value = aws_eip.ec2_eip.public_ip
 }
