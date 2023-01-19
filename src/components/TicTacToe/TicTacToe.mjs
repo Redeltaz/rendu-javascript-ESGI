dcef55038552c483ac28dd02d5b123304a97ce4a
Redeltaz <lucascampistron95@gmail.com>
2023-01-19 19:14:50 +0100

    wip: add main tictactoe logic

--- src/components/TicTacToe/TicTacToe.mjs
+++ src/components/TicTacToe/TicTacToe.mjs
@@ -13,17 +13,65 @@ export class TicTacToe extends HTMLElement {
         style.textContent = css;
         shadow.appendChild(style);
 
-        const buttons = shadow.querySelectorAll(".game-button")
-        const players = ["X", "O"]
+        this.result = shadow.getElementById("result")
+        this.buttons = shadow.querySelectorAll(".game-button")
         this.turn = 0
+        this.remainingTurn = 9
+        this.winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
 
-        console.log(buttons)
-        buttons.forEach((button) => {
-            button.addEventListener("click", () => {
-                button.innerHTML = this.turn % 2 === 0 ? "X" : "O"
-                this.turn++
-            })
+        this.result.textContent = "X player turn"
+        this.buttons.forEach((button) => {
+            button.addEventListener("click", this.newTurn)
         })
     }
 
+    newTurn = (buttonEvent) => {
+        const button = buttonEvent.target;
+        const player = this.turn % 2 === 0 ? "X" : "O"
+
+        button.textContent = player
+        this.turn++
+
+        button.removeEventListener("click", this.newTurn)
+
+        if(this.checkWinner(player)) {
+            //this.result.textContent = `${player} won the game !`
+            alert(`${player} won the game !`)
+            this.resetGame()
+        } else if(!this.remainingTurn) {
+            alert("It's a draw")
+            this.resetGame()
+        } else {
+            this.result.textContent = `${player === "X" ? "O" : "X"} player turn`
+            this.remainingTurn--
+        }
+    }
+
+    checkWinner = (player) => {
+        const isWinner = this.winningConditions.some(condition => {
+            return condition.every(index => {
+                return this.buttons[index].textContent.includes(player)
+            })
+        }) 
+
+        return isWinner
+    }
+
+    resetGame = () => {
+        this.turn = 0
+        this.remainingTurn = 9
+        this.buttons.forEach(button => {
+            button.textContent = ""
+            button.addEventListener("click", this.newTurn)
+        })
+    }
 }
